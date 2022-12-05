a3c1915450200d49d42e65b76a497d1100e28a78
Burundukov Denis <burundukov.d@yandex.ru>
2022-12-05 10:19:08 +0500

    Task 41 completed

---
+++ Task 41/Program.cs
@@ -0,0 +1,37 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void OverNull()
+        {
+            Console.WriteLine("Введите любые числа, для окончания ввода и начала подсчёта введите стоп-число 123");
+
+            int count = 0;
+            int num = 0;
+
+
+            while (num != 123)
+
+            {
+                Console.WriteLine("Введите любое число: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > 0 && num != 123)
+                {
+                    count++;
+
+                }
+                else if (num == 123)
+                {
+                    Console.WriteLine($"Введено стоп-число 123. \n" + $"Всего введено {count} значений больше нуля");
+                    break;
+                }
+
+            }
+
+        }
+
+        OverNull();
+    }
+}
---
+++ Task 43/Program.cs
@@ -0,0 +1,37 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void CrossPoint(double b1, double k1, double b2, double k2)
+        {
+
+            if (k1 != k2)
+            {
+                double x = (b1 - b2) / (k2 - k1);
+                double y = k1 * x + b1;
+
+                Console.Write($"Точка пересечения ({x}; {y})");
+            }
+            else
+            {
+                Console.Write("Точка пересечения отсутствует");
+            }
+
+        }
+        Console.WriteLine("Дано уравненение: y = k1 * x + b1, y = k2 * x + b2, введите данные чтобы найти точку пересечения двух прямых");
+        Console.WriteLine("Введите b1: ");
+        double b1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите k1: ");
+        double k1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите b2: ");
+        double b2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите k2: ");
+        double k2 = Convert.ToInt32(Console.ReadLine());
+
+        CrossPoint(b1, k1, b2, k2);
+    }
+}
